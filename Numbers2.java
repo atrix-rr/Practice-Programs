81ee4879e9e6bb06e8129b31fc15fb607674b444
Anant Tiwari <ananttiwari231@gmail.com>
2026-07-24 16:35:09 +0530

    updated

---
+++ Numbers2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Numbers2 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+
+        int i = 20;
+        while (i >= 1) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
