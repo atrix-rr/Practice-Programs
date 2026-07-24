04c9ad15ee55b9c64f9b1f6592fc766dfe83f478
Anant Tiwari <ananttiwari231@gmail.com>
2026-07-24 17:47:02 +0530

    updatd

---
+++ Pattern1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Pattern1 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int i, j, a = 1;
+        for (i = 1; i <= 4; i++) {
+            for (j = 1; j <= 1; j++) {
+                System.out.print(a);
+                a++;
+            }
+
+        }
+    }
+}
