424497f4489f5f7400dc4bd4d0a7d58be2933354
Anant Tiwari <ananttiwari231@gmail.com>
2026-07-24 16:42:58 +0530

    updated

---
+++ Even.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Even {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int i = 1;
+        while (i <= 50) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+                i++;
+            }
+        }
+
+    }
+}
