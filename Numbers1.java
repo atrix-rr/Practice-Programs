bbcf331009e6edd5b5ddb985763610489f456025
Anant Tiwari <ananttiwari231@gmail.com>
2026-07-24 16:31:38 +0530

    updated

---
+++ Numbers1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Numbers1 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+
+        int i = 1;
+        while (i <= 20) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
